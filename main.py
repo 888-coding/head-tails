adf474b88c4deb9c6103e283efe5153d8a22e4be
888-coding <pachelee@gmail.com>
2023-02-20 19:59:11 -0300

    main.py add lines

--- main.py
+++ main.py
@@ -3,3 +3,15 @@
 # Learning random 
 
 import random
+
+random_number = random.randint(0,1)
+
+print("Welcome to the Head os tail app")
+print("Here is autoprogrammed to response the valeu")
+
+#Condition
+if random_number == 1 :
+    print("Head")
+else:
+    print("Tail")
+
